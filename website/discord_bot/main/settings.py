b7344e69d936c5b6b118d8441201a298a5cc1534
Tom Stevens <tomjstevens9@gmail.com>
2022-01-26 17:21:49 +0000

    Minor changes after first deployment

--- website/discord_bot/main/settings.py
+++ website/discord_bot/main/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'hal69000.fun']
 
 # Application definition
 
