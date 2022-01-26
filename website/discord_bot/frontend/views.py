63b316f3157ba78d1af5e1898755942554c436da
Tom Stevens <tomjstevens9@gmail.com>
2022-01-26 17:36:05 +0000

    Added endpoint for sending commands to the bot from the webpage

--- website/discord_bot/frontend/views.py
+++ website/discord_bot/frontend/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Sound
 from .forms import SoundForm
+
+import pika
 
 @login_required
 def index(request):
@@ -19,3 +24,17 @@ def index(request):
         'form': form,
     }
     return render(request, 'frontend/index.html', context)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def put_command_on_queue(request):
+    # Simple code for adding a command onto the queue
+    # TODO check that it is a valid command before putting on queue
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare(queue='commands')
+    channel.basic_publish(exchange='',
+                          routing_key='commands',
+                          body=request.data['command'])
+    connection.close()
+    return Response(request.data)
