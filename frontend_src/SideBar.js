08f38730c47f8276ee9170d2cdc78ffad0a2eae9
Tom Stevens <tomjstevens9@gmail.com>
2022-07-07 23:25:14 +0100

    Restored disco mode

--- frontend_src/SideBar.js
+++ frontend_src/SideBar.js
@@ -2,12 +2,13 @@ import Mario from './mario.webp'
 
 import SoundUploader from './SoundUploader.js'
 import PopularTags from './PopularTags.js'
+import { disco } from './random'
 
 function SideBar (props) {
   return (
     <div id='side-bar'>
       <div id='side-bar-inner'>
-        <img src={Mario} />
+        <img src={Mario} id='image-header' onClick={disco} />
         <SoundUploader />
         <PopularTags />
       </div>
---
+++ frontend_src/random.js
@@ -0,0 +1,7 @@
+const disco = () => {
+  document.body.classList.add('disco')
+  const headerElement = document.getElementById('image-header')
+  headerElement.classList.add('header-disco')
+}
+
+export { disco }
